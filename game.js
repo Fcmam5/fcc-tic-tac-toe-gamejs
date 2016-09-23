76b33629a5c6a1975880f6e8add2ae5d51052e6f
fcmam5 <dr.fcmam5@gmail.com>
2016-09-23 20:31:03 +0100

    player1 can check boxes but cannot delete them from the list

--- game.js
+++ game.js
@@ -1,41 +1,78 @@
-// MyArray
-var playGround = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]; // 3*3
-
 // Wining cases
 var winingCases = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [7,5,3],
-    [3,5,7],
-    [9,5,1]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [7, 5, 3],
+    [3, 5, 7],
+    [9, 5, 1]
 ];
+
+var uncheckedCases = [7, 8, 9, 4, 5, 6, 1, 2, 3];
+
+// plyaingProcess
+var playGroundMgt = function (theCase) {
+	'use strict';
+	var myIndex = uncheckedCases.indexOf[theCase];
+	console.log("--- playground n" + theCase + "---")
+	console.log("- you : "+ theCase);
+	console.log("deleting: " + myIndex);
+	console.log("before: " + uncheckedCases);
+	
+	uncheckedCases.splice(myIndex, 1);
+	console.log("after: " + uncheckedCases);
+	
+	console.log("----");
+
+}
+
 
 //You, the first player
 var youThePlayer = []; // moves
 
-var youPlay = function(x,y) {
-    // playGround is not zero indexed :p
-    youThePlayer.push(playGround[x-1][y-1]);
+var youPlay = function (x) {
+	'use strict';
+	// Append to ur checked boxes
+	youThePlayer.push(x);
 };
+
+var iPlay = function (callback) {
+	// When the user clicks on the case
+	$("td").on('click', function (event) {
+		// CHeck me
+		$(this).html("X");
+		$(this).addClass("disabledCase");
+		//Add score
+		var numbr = parseInt(event.target.id);
+		youPlay(numbr);
+		playGroundMgt(numbr);
+		console.log("You played: " + youThePlayer);
+		console.log("Availible cases " + uncheckedCases);
+	}); // end playing event :)
+
+	callback();
+}
 
 //computer
 var computerPlayer = [];
 
-var smartMove = function() {
-    var available = [];
-    //todo : check the nearby available case
-    // Random
-    // maybe: close it for u
-    // computerPlayer.push();
+var smartMove = function () {
+	'use strict';
+	var available = [];
+	//todo : check the nearby available case
+	// Random
+	// maybe: close it for u
+	// computerPlayer.push();
 };
 
-// Playing :
+//Jquery
+$(document).ready(function () {
+
+	iPlay(function () {
+		console.log("yo");
+	})
+});
